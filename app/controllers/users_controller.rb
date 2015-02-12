e742c7bd0a338907bd57eb7016b7db1516f9af6c
vladislavpauk <vladislavpauk@gmail.com>
2015-02-12 15:07:11 +0200

    v1.0.1-alpha4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 	def pay_out
 		respond_to do |format|
-			if UserReferralManager.new(current_user).pay_out(current_user.decorate.referral_balance*0.995)
-				format.html { redirect_to :back }
-				format.json { head :no_content }
-			else
-				format.html { redirect_to :back, alert: 'Вывести деньги не получилось. Возникла ошибка. Мы уже ее решаем' }
-				format.json { head :no_content }
+			if current_user.decorate.referral_balance > 0
+				if UserReferralManager.new(current_user).pay_out(current_user.decorate.referral_balance*0.995)
+					format.html { redirect_to :back }
+					format.json { head :no_content }
+				else
+					format.html { redirect_to :back, alert: 'Вывести деньги не получилось. Возникла ошибка. Мы уже ее решаем' }
+					format.json { head :no_content }
+				end
 			end
 		end
 	end
